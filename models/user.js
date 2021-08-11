cd18f506d7817a593c213a597d786687fc9dafa8
Gremwiz1 <gremwiz@yandex.ru>
2021-08-11 11:54:53 +0300

    Added email and password in model user. Add validator email

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -16,6 +17,20 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: "Некорректная почта",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
   },
 });
 module.exports = mongoose.model("user", userSchema);
