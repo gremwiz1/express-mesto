e2fc4cb8d663b0135602f507e2d831780f5f8720
Gremwiz1 <gremwiz@yandex.ru>
2021-08-12 22:09:09 +0300

    Added crash-test

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ mongoose.connect("mongodb://localhost:27017/mestodb", {
 });
 app.use(limiter);
 app.use(requestLogger); // подключаем логгер запросов
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
 app.post("/signin", celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
