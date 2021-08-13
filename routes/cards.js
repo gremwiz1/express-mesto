fe2ea7f545d94f007bebe4588742bbb5265a75cc
Gremwiz1 <gremwiz@yandex.ru>
2021-08-13 14:58:02 +0300

    Added /api in urls routes

--- routes/cards.js
+++ routes/cards.js
@@ -13,24 +13,24 @@ const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require("../controllers/cards");
 
-router.post("/cards", celebrate({
+router.post("/api/cards", celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
     link: Joi.string().required().custom(method),
   }),
 }), createCard);
-router.get("/cards", getCards);
-router.delete("/cards/:cardId", celebrate({
+router.get("/api/cards", getCards);
+router.delete("/api/cards/:cardId", celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).hex(),
   }),
 }), deleteCard);
-router.put("/cards/:cardId/likes", celebrate({
+router.put("/api/cards/:cardId/likes", celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).hex(),
   }),
 }), likeCard);
-router.delete("/cards/:cardId/likes", celebrate({
+router.delete("/api/cards/:cardId/likes", celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).hex(),
   }),
