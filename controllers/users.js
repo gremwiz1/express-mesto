5dabdf0636b57ad80c74b194ac85e0c34bca41af
Gremwiz1 <gremwiz@yandex.ru>
2021-08-14 00:27:21 +0300

    Change jwt_secret

--- controllers/users.js
+++ controllers/users.js
@@ -100,12 +100,12 @@ module.exports.updateAvatar = (req, res, next) => {
 };
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
-  const { NODE_ENV, JWT_SECRET } = process.env;
+  const { JWT_SECRET = "dev-key" } = process.env;
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV === "production" ? JWT_SECRET : "dev-key",
+        JWT_SECRET,
         { expiresIn: "7d" },
       );
       res.status(200).send({ token });
