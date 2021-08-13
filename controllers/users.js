d6bbac4e56d5c2fdd020dd1c5e4a1bb84a7b7f1d
Gremwiz1 <gremwiz@yandex.ru>
2021-08-13 19:56:57 +0300

    Added upsert: false

--- controllers/users.js
+++ controllers/users.js
@@ -60,7 +60,8 @@ module.exports.getUser = (req, res, next) => {
 };
 module.exports.updateUser = (req, res, next) => {
   const { name, about } = req.body;
-  User.findOneAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+  User.findOneAndUpdate(req.user._id, { name, about },
+    { new: true, runValidators: true, upsert: false })
     .orFail(new Error("NotValidIdUser"))
     .then((user) => {
       res.status(200).send(user);
@@ -80,7 +81,8 @@ module.exports.updateAvatar = (req, res, next) => {
   if (!avatar) {
     throw new BadRequestError("Поле аватар не заполнено");
   } else {
-    User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+    User.findByIdAndUpdate(req.user._id, { avatar },
+      { new: true, runValidators: true, upsert: false })
       .orFail(new Error("NotValidIdUser"))
       .then((user) => {
         res.status(200).send(user);
